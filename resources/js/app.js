341a9ed991400ee5e3c8b7d4e44eea40355e1773
Jitesh Bharadia <jbharadia@live.com>
2016-10-15 12:29:53 +0100

    Time logic

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+var today = newDate()
+var curHr = today.getHours()
+
+if(curHr<12 {
+	console.log("Good Morning")
+} else if (curHr>18) {
+	console.log("Good Afternoon")
+} else {console.log("Good Evening")}
